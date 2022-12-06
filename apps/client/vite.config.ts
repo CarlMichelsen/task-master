05d64146f8015be5b8d9267ae8b035c97fd30d30
Carl Lilholm Michelsen <carl.lilholm@protonmail.com>
2022-12-06 16:39:47 +0100

    Working on ci/cd autimatic build.

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    outDir: "../server/src/public",
-    emptyOutDir: true
-  }
+	plugins: [svelte()],
+	build: {
+		outDir: "./dist",
+		emptyOutDir: true,
+	},
 });
