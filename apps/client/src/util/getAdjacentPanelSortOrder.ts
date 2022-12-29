355f8166eeba5bdeb502d1c35fbdd4a5914b4021
Carl Lilholm Michelsen <carl.lilholm@protonmail.com>
2022-12-29 21:42:20 +0100

    Fixed move panels on edges.

--- apps/client/src/util/getAdjacentPanelSortOrder.ts
+++ apps/client/src/util/getAdjacentPanelSortOrder.ts
@@ -8,6 +8,8 @@ export const getAdjacentPanelSortOrder = (
 	if (panels.length <= 1) NaN;
 	const panelToMoveId = panels.findIndex((p) => p.id === panelId);
 	if (panelToMoveId === -1) return NaN;
+	if (panelToMoveId === 0) return NaN;
+	if (panelToMoveId === panels.length) return NaN;
 
 	const nextPanel = panels[panelToMoveId + direction];
 	let newOrder = nextPanel.sortOrder + 500 * direction;
